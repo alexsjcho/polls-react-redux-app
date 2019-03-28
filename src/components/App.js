5ad099778eb3d148cd028696f17d34656565eb86
Alex Cho <alexsjcho@gmail.com>
2019-03-27 19:14:16 -0700

    added AddPoll component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
 import LoadingBar from "react-redux-loading";
 import Leaderboard from "./Leaderboard";
+import AddPoll from "./AddPoll";
 
 class App extends Component {
   componentDidMount() {
@@ -13,7 +14,7 @@ class App extends Component {
     return (
       <div>
         <LoadingBar />
-        {this.props.loading === true ? null : <Leaderboard />}
+        {this.props.loading === true ? null : <AddPoll />}
       </div>
     );
   }
