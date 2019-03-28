2aafe0d41cc69158455b668cc6ddbe185f463ef9
Alex Cho <alexsjcho@gmail.com>
2019-03-27 19:06:09 -0700

    added Leadership component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
 import LoadingBar from "react-redux-loading";
+import Leaderboard from "./Leaderboard";
 
 class App extends Component {
   componentDidMount() {
@@ -12,7 +13,7 @@ class App extends Component {
     return (
       <div>
         <LoadingBar />
-        {this.props.loading === true ? null : <Dashboard />}
+        {this.props.loading === true ? null : <Leaderboard />}
       </div>
     );
   }
