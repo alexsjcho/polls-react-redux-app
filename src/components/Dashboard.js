fce489779fc7883c84364fe62a7b6ebd52c5adfe
Alex Cho <alexsjcho@gmail.com>
2019-03-27 20:08:00 -0700

    added React Router

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Dashboard extends Component {
   state = {
@@ -42,7 +43,9 @@ class Dashboard extends Component {
         </div>
         <ul className="dashboard-list">
           {list.map(poll => (
-            <li key={poll.id}>{poll.question}</li>
+            <li key={poll.id}>
+              <Link to={`polls/${poll.id}`}>{poll.question}</Link>
+            </li>
           ))}
         </ul>
       </div>
