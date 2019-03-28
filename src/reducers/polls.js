3f63678b075e085885bb02b7ebd153cf756de63c
Alex Cho <alexsjcho@gmail.com>
2019-03-27 19:26:27 -0700

    added Poll to Store

---
+++ src/reducers/polls.js
@@ -0,0 +1,18 @@
+import { RECEIVE_POLLS, ADD_POLL } from "../actions/polls";
+
+export default function polls(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_POLLS:
+      return {
+        ...state,
+        ...action.polls
+      };
+    case ADD_POLL:
+      return {
+        ...state,
+        [action.poll.id]: action.poll
+      };
+    default:
+      return state;
+  }
+}
---
+++ src/reducers/users.js
@@ -0,0 +1,25 @@
+import { RECEIVE_USERS } from "../actions/users";
+import { ADD_POLL } from "../actions/polls";
+
+export default function users(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_USERS:
+      return {
+        ...state,
+        ...action.users
+      };
+    case ADD_POLL:
+      const poll = action.poll;
+      const { author, id } = poll;
+
+      return {
+        ...state,
+        [author]: {
+          ...state[author],
+          polls: state[author].polls.concat([id])
+        }
+      };
+    default:
+      return state;
+  }
+}
