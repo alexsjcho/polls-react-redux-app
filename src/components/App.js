7773307f23bb8a0835c392fb73a7cd9f149fe987
Alex Cho <alexsjcho@gmail.com>
2019-03-27 19:36:37 -0700

    added Poll state logi

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./Dashboard";
 import LoadingBar from "react-redux-loading";
 import Leaderboard from "./Leaderboard";
 import AddPoll from "./AddPoll";
+import Poll from "./Poll";
 
 class App extends Component {
   componentDidMount() {
@@ -14,7 +15,9 @@ class App extends Component {
     return (
       <div>
         <LoadingBar />
-        {this.props.loading === true ? null : <AddPoll />}
+        {this.props.loading === true ? null : (
+          <Poll match={{ params: { id: "loxhs1bqm25b708cmbf3g" } }} />
+        )}
       </div>
     );
   }
